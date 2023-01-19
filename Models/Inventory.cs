43275c36074177141b6be16bb2e358e5af16908f
clems67 <caravita.clement.67150@gmail.com>
2023-01-19 22:12:57 +0100

    add Inventory Model class

---
+++ Models/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetEnglishP5_3.Models
+{
+    public class Inventory
+    {
+        [Key]
+        //[BindNever]
+        public int Id { get; set; }
+        [Required]
+        [RegularExpression(@"[\d\w]{12}", ErrorMessage = "12 char code")]
+        public string VIN { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public string Make { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public string Trim { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime PurchaseDate { get; set; }
+        [Required]
+        public float PurchasePrice { get; set; }
+        public string? Repairs { get; set; }
+        public float? RepairCost { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? LotDate { get; set; }
+        public float? SellingPrice { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? SaleDate { get; set; }
+        public string Picture { get; set; }
+    }
+}
