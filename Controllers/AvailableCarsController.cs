cac65fb5c3c376b29c4eca1575c9e3b5bff11c07
clems67 <caravita.clement.67150@gmail.com>
2023-01-19 23:10:32 +0100

    add AvailableCarsController + its vue + its link in the layout
    minor changes

---
+++ Controllers/AvailableCarsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DotNetEnglishP5_3.Data;
+using DotNetEnglishP5_3.Models;
+
+namespace DotNetEnglishP5_3.Controllers
+{
+    public class AvailableCarsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AvailableCarsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AvailableCars
+        public async Task<IActionResult> Index()
+        {
+            return _context.Inventory != null ?
+                        View(await _context.Inventory
+                            .Where(items => items.PurchaseDate != DateTime.MinValue)
+                            .ToListAsync()) :
+                        Problem("Entity set 'ApplicationDbContext.Inventory'  is null.");
+        }
+
+    }
+}
