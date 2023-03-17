17f0592df8dcf69aa2ab3ee525e06e558445f111
clems67 <caravita.clement.67150@gmail.com>
2023-03-17 09:44:45 +0100

    add Role Manager

--- Controllers/UserRolesController.cs
+++ Controllers/UserRolesController.cs
@@ -1,7 +1,10 @@
 ﻿using DotNetEnglishP5_3.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Collections;
 
 namespace DotNetEnglishP5_3.Controllers
 {
@@ -14,7 +17,7 @@ namespace DotNetEnglishP5_3.Controllers
             _roleManager = roleManager;
             _userManager = userManager;
         }
-
+        [Authorize(Roles = "SuperAdmin")]
         public async Task<IActionResult> Index()
         {
             var users = await _userManager.Users.ToListAsync();
@@ -22,6 +25,7 @@ namespace DotNetEnglishP5_3.Controllers
             foreach (IdentityUser user in users)
             {
                 var thisViewModel = new UserRolesViewModel();
+                thisViewModel.UserId = user.Id;
                 thisViewModel.Email = user.Email;
                 //thisViewModel.FirstName = user.FirstName;
                 //thisViewModel.LastName = user.LastName;
@@ -29,6 +33,49 @@ namespace DotNetEnglishP5_3.Controllers
                 userRolesViewModel.Add(thisViewModel);
             }
             return View(userRolesViewModel);
+        }
+        [Authorize(Roles = "SuperAdmin")]
+        public async Task<IActionResult> Manage(string userId)
+        {
+            ViewBag.userId = userId;
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {userId} cannot be found";
+                return View("NotFound");
+            }
+            ViewBag.UserName = user.UserName;
+            var model = new List<SelectListItem>();
+            foreach (var role in _roleManager.Roles)
+            {
+                model.Add(new SelectListItem { Text = role.Name, Value = role.Id });
+            }
+            return View(model);
+        }
+        [Authorize(Roles = "SuperAdmin")]
+        [HttpPost]
+        public async Task<IActionResult> Manage(ManageUserRolesViewModel model, string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return View();
+            }
+            var roles = await _userManager.GetRolesAsync(user);
+            var result = await _userManager.RemoveFromRolesAsync(user, roles);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Cannot remove user existing roles");
+                return View(model);
+            }
+            var roleToAdd =  _roleManager.Roles.Where(r => r.Id == model.RoleId).First().Name;
+            result = await _userManager.AddToRoleAsync(user, roleToAdd);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Cannot add selected roles to user");
+                return View(model);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Models/ManageUserRolesViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetEnglishP5_3.Models
+{
+    public class ManageUserRolesViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
