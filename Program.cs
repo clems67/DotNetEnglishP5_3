7c33c1b07af54c5479b4e950bd811d2c1f04bf97
clems67 <caravita.clement.67150@gmail.com>
2023-04-07 16:16:24 +0200

    azure app + azure db

--- Program.cs
+++ Program.cs
@@ -2,8 +2,12 @@ using DotNetEnglishP5_3.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
