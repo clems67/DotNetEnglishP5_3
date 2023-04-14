af76a6194ee4d1edd1e2626b5d74913327b36369
clems67 <caravita.clement.67150@gmail.com>
2023-04-14 07:41:08 +0200

    delete keyvault

--- Program.cs
+++ Program.cs
@@ -6,9 +6,6 @@ using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
-
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
