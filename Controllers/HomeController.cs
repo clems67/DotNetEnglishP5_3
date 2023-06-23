1c66246b8c1fd2adb845c227c054b614f4efdb63
clems67 <caravita.clement.67150@gmail.com>
2023-06-23 15:08:14 +0200

    available cars on the home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using DotNetEnglishP5_3.Models;
+﻿using DotNetEnglishP5_3.Data;
+using DotNetEnglishP5_3.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace DotNetEnglishP5_3.Controllers
@@ -7,15 +9,47 @@ namespace DotNetEnglishP5_3.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return _context.Inventory != null ?
+            View(await _context.Inventory
+                .Where(items => items.PurchaseDate != DateTime.MinValue)
+                .Where(items => items.RepairCost != null)
+                .ToListAsync()) :
+            Problem("Entity set 'ApplicationDbContext.Inventory'  is null.");
+        }
+
+        // GET: Home/Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            Inventory inventory = new Inventory();
+            try
+            {
+                if (id == null || _context.Inventory == null)
+                {
+                    return NotFound();
+                }
+
+                inventory = await _context.Inventory
+                    .FirstOrDefaultAsync(m => m.Id == id);
+                if (inventory == null)
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("InventoryController Create :", e);
+            };
+            return View(inventory);
         }
 
         public IActionResult Privacy()
